799ca5f81878bbc94be105e4dc143717162d3191
jarrettbutler <jbutler98@hotmail.com>
2023-01-21 13:48:23 -0400

    Trying to push to heroku

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
+//Creates a port or defaults to local 3000
 const PORT = process.env.port || 3001;
 
 const app = express();
 
+//middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
